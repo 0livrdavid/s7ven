6b415262e34754a03487598aea9f54bc841efbe8
David <david@cvssistemas.com.br>
2021-09-18 14:51:42 -0300

    .

--- app/src/main/java/br/com/application/s7ven/MainActivity.java
+++ app/src/main/java/br/com/application/s7ven/MainActivity.java
@@ -2,13 +2,35 @@ package br.com.application.s7ven;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import br.com.application.s7ven.ActivityLogin.Login;
+import br.com.application.s7ven.ActivityLogin.Redirect_Login_Register;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(currentuser == null) {
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
