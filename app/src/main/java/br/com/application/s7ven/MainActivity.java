68763a7bfec3bcc6b839efc16ad7d1710899b20e
David <david@cvssistemas.com.br>
2021-09-18 13:00:12 -0300

    Initial commit

---
+++ app/src/main/java/br/com/application/s7ven/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.application.s7ven;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
