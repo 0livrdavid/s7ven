e44f0bfd7f65bac1dc4305a02368a166018cb6c5
David <david@cvssistemas.com.br>
2021-09-18 17:42:32 -0300

    .

--- app/src/main/java/br/com/application/s7ven/ActivityLogin/Login.java
+++ app/src/main/java/br/com/application/s7ven/ActivityLogin/Login.java
@@ -1,16 +1,81 @@
 package br.com.application.s7ven.ActivityLogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import br.com.application.s7ven.Dashboard;
 import br.com.application.s7ven.R;
 
 public class Login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private EditText edt_email;
+    private EditText edt_senha;
+    private Button btn_login;
+    private ProgressBar loginProgressBar;
+    private CheckBox ckb_mostrar_senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        edt_email = findViewById(R.id.edt_email);
+        edt_senha = findViewById(R.id.edt_senha);
+        btn_login = findViewById(R.id.btn_login);
+        loginProgressBar = findViewById(R.id.loginProgressBar);
+        ckb_mostrar_senha = findViewById(R.id.ckb_mostrar_senha);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String loginEmail = edt_email.getText().toString();
+                String loginSenha = edt_senha.getText().toString();
+
+                if(!TextUtils.isEmpty(loginEmail) || !TextUtils.isEmpty(loginSenha)){
+                    loginProgressBar.setVisibility(View.VISIBLE);
+                    mAuth.signInWithEmailAndPassword(loginEmail,loginSenha)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        abrirTelaDashboard();
+                                    } else {
+                                        String error = task.getException().getMessage();
+                                        Toast.makeText(Login.this, ""+error, Toast.LENGTH_SHORT).show();
+                                        loginProgressBar.setVisibility(View.INVISIBLE);
+                                    }
+                                }
+                            });
+
+                }
+            }
+        });
+
+
+    }
+
+    private void abrirTelaDashboard(){
+        Intent intent = new Intent(Login.this, Dashboard.class);
+        startActivity(intent);
+        finish();
     }
 }
