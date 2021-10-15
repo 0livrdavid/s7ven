75cb837da7f5cef2ea437d13c09a141937639879
David <david@cvssistemas.com.br>
2021-10-14 22:10:10 -0300

    .

--- app/src/main/java/br/com/application/s7ven/fragments/home/HomeFragment.java
+++ app/src/main/java/br/com/application/s7ven/fragments/home/HomeFragment.java
@@ -1,4 +1,4 @@
-package br.com.application.s7ven.ui.home;
+package br.com.application.s7ven.fragments.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,7 +12,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import br.com.application.s7ven.R;
 import br.com.application.s7ven.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
--- app/src/main/java/br/com/application/s7ven/fragments/notifications/NotificationsFragment.java
+++ app/src/main/java/br/com/application/s7ven/fragments/notifications/NotificationsFragment.java
@@ -1,4 +1,4 @@
-package br.com.application.s7ven.ui.notifications;
+package br.com.application.s7ven.fragments.notifications;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,7 +12,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import br.com.application.s7ven.R;
 import br.com.application.s7ven.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
