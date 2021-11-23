7ff22ace9ce02254ff0ae8912996bb3a08ce6845
David <maxlex9854@gmail.com>
2021-11-23 19:07:45 -0300

    .

--- app/src/main/java/br/com/application/s7ven/fragments/configuracao/ConfiguracaoFragment.java
+++ app/src/main/java/br/com/application/s7ven/fragments/configuracao/ConfiguracaoFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
@@ -42,6 +43,17 @@ public class ConfiguracaoFragment extends Fragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         periodos.setAdapter(adapter);
+        periodos.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         return root;
     }
